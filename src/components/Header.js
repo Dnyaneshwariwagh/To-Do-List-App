c9e9e60cdccbd6b3169a30466b7492c6db9f3944
Dnyaneshwariwagh <dnyaneshwariwagh115@gmail.com>
2024-07-02 23:21:53 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1> To-Dos List</h1>
+        <p> reminds everything</p>
+      
+    </div>
+  );
+};
+
+export default Header;
